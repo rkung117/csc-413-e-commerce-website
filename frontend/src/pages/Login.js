b932919d7624dca464fc4eca4a94a28e14021212
Saksham <smanandhar96@gmail.com>
2021-05-13 22:47:43 -0700

    Frontend Files created and/or updated

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import axios from 'axios';
+
+const Login = ({ws}) => {
+    return (
+      <div>
+        <h1>Login</h1>
+        <div>
+            <section>Email</section>
+            <input />
+        </div>
+      </div>
+    );
+  };
+
+export default Login;
---
+++ frontend/src/pages/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import axios from 'axios';
+
+const Post = ({ws}) => {
+    return (
+      <div>
+        <h1>Post</h1>
+        <div>
+            <section>Email</section>
+            <input />
+        </div>
+      </div>
+    );
+  };
+
+export default Post;
