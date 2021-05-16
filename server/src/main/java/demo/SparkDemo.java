aaab7e4823b702d9e2224f32192f423150a05f08
rkung117 <rkung117@gmail.com>
2021-05-15 21:09:26 -0700

    added spark and websockets to test my react

--- server/src/main/java/demo/SparkDemo.java
+++ server/src/main/java/demo/SparkDemo.java
@@ -4,14 +4,40 @@ import static spark.Spark.*;
 import static com.mongodb.client.model.Filters.*;
 
 
+import com.google.gson.Gson;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.sun.tools.javac.util.List;
+import java.util.List;
 import java.util.ArrayList;
 import org.bson.Document;
 
 public class SparkDemo {
+
+  static List<MessageDto> listingList = new ArrayList<>();
+  static Gson gson = new Gson();
+
   public static void main(String[] args) {
+    port (1235);
+    webSocket("/ws", WebSocketHandler.class);
+
+    // why do post and get still work even though endpoints are different
+    post("/submit-listing", (req, res) -> {
+      String bodyString = req.body();
+      System.out.println(bodyString);
+      MessageDto newListing = gson.fromJson(bodyString, MessageDto.class);
+      listingList.add(newListing);
+
+      // alert all other clients that something has changed
+      BroadcastDto broadcastDto = new BroadcastDto(listingList,
+              WebSocketHandler.getClientCount());
+      WebSocketHandler.broadcast((gson.toJson(broadcastDto)));
+
+      return listingList.size();
+    });
+
+    get("/get-listing", (req, res) -> {
+      return gson.toJson(listingList);
+    });
   }
 }
