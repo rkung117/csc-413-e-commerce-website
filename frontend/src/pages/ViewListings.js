5e92f95ff801d3eda23c292ff0d1a1a03fbe599f
rkung117 <rkung117@gmail.com>
2021-05-15 18:39:25 -0700

    copied post listing logic but removed abilty to post

--- frontend/src/pages/ViewListings.js
+++ frontend/src/pages/ViewListings.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import axios from'axios';
 
-const ViewListings = () => {    
+const ViewListings = ({ ws }) => { 
+    
+    const [message, setMessage] = React.useState('');
+    const [messageList, setMessageList] = React.useState([]);
+
+    const handleMessageUpdate = (e) => {
+        setMessage(e.target.value);
+    };
+    
+    const handleSubmit = () => {
+        console.log(message);
+        const body = {
+            message : message,
+        };
+        axios.post('/submit-listing', body)
+            .then(fetchMessages);
+        setMessage('');
+    };
+
+    const fetchMessages = () => {
+        axios.get('/get-listing') //asyc
+        .then((res) => {
+            // res is what the spark server sent back
+            console.log(res.data);
+            setMessageList(res.data); // save for using on the page
+        });
+    };
+
+    React.useEffect(() => {
+        // Trigger only 1 time
+        fetchMessages();
+
+        // listen for ws here
+        ws.addEventListener('message', (message) => {
+            console.log(message);
+            const parsedData = JSON.parse(message.data);
+            console.log(parsedData);
+            setMessageList(parsedData.messages); // triggers the refresh
+        });
+    }, []);
+
     return (
         <div>
-            <h1>View Listings</h1>            
+            <h1>View Listings</h1>
+            {/* <input value={message} onChange={handleMessageUpdate}/>
+            <button onClick={handleSubmit}>Submit</button> */}
+
+            <div>
+                {messageList.map((object, i) => <div key={i}>{object.message}</div>)}
+            </div>       
         </div>
     );
 };
