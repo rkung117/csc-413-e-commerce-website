d9f633a702fafdef724f16edb8d63b837cd805f7
rkung117 <rkung117@gmail.com>
2021-05-16 17:30:44 -0700

    added editable text fields for type, price, and title to post listings that send data to server

--- frontend/src/pages/PostListings.js
+++ frontend/src/pages/PostListings.js
@@ -2,29 +2,65 @@ import axios from 'axios';
 import React from 'react';
 
 // watch week 10 classwork 9 - 90 min mark for sending info to backend
+// to handle email input, should i hide ability to post and have it reveal upon email input
+// how do i determine if email input is valid or not?
+
+// watch week 11 classwork 10 - 97 min mark for intro to hw 3
 const PostListings = ({ ws }) => { // props incoming in the function
     // text in the box
-    const [message, setMessage] = React.useState('');
+    const [message, setMessage] = React.useState(''); // message will be description for this part
+    const [type, setType] = React.useState('');
+    const [title, setTitle] = React.useState('');
+    const [price, setPrice] = React.useState('');
     const [email, setEmail] = React.useState('');
     // list of messages
     const [messageList, setMessageList] = React.useState([]);
+    const [typeList, setTypeList] = React.useState([]);
+    const [priceList, setPriceList] = React.useState([]);
+    const [titleList, setTitleList] = React.useState([]);
+    
 
     const handleMessageUpdate = (e) => {
         setMessage(e.target.value);
+    };
+
+    const handleTypeUpdate = (e) => {
+        setType(e.target.value);
+    };
+
+    const handleTitleUpdate = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handlePriceUpdate = (e) => {
+        setPrice(e.target.value);
     };
 
     const handleEmail = (e) => {
         setEmail(e.target.value);
     };
 
+    const handleLogin = () => {
+        setEmail('');
+    };
+
     const handleSubmit = () => {
         console.log(message);
+        console.log(type);
+        console.log(price);
+        console.log(title);
         const body = {
             message : message,
+            type : type,
+            price : price,
+            title : title,
         };
         axios.post('/submit-listing', body)
             .then(fetchMessages);
         setMessage('');
+        setType('');
+        setPrice('');
+        setTitle('');
     };
 
     const fetchMessages = () => {
@@ -33,6 +69,9 @@ const PostListings = ({ ws }) => { // props incoming in the function
             // res is what the spark server sent back
             console.log(res.data);
             setMessageList(res.data); // save for using on the page
+            setTypeList(res.data);
+            setPriceList(res.data);
+            setTitleList(res.data);
         });
     };
 
@@ -43,9 +82,30 @@ const PostListings = ({ ws }) => { // props incoming in the function
         // listen for ws here
         ws.addEventListener('message', (message) => {
             console.log(message);
-            const parsedData = JSON.parse(message.data);
-            console.log(parsedData);
-            setMessageList(parsedData.messages); // triggers the refresh
+            const parsedMessageData = JSON.parse(message.data);
+            console.log(parsedMessageData);
+            setMessageList(parsedMessageData.messages); // triggers the refresh
+        });
+
+        ws.addEventListener('type', (type) => {
+            console.log(type);
+            const parsedTypeData = JSON.parse(type.data);
+            console.log(parsedTypeData);
+            setTypeList(parsedTypeData.type); // triggers the refresh
+        });
+
+        ws.addEventListener('price', (price) => {
+            console.log(price);
+            const parsedPriceData = JSON.parse(price.data);
+            console.log(parsedPriceData);
+            setPriceList(parsedPriceData.price); // triggers the refresh
+        });
+
+        ws.addEventListener('title', (title) => {
+            console.log(title);
+            const parsedTitleData = JSON.parse(title.data);
+            console.log(parsedTitleData);
+            setTitleList(parsedTitleData.title); // triggers the refresh
         });
     }, []);
     
@@ -55,9 +115,13 @@ const PostListings = ({ ws }) => { // props incoming in the function
 
             <h2>Enter Email</h2>
             <input value={email} onChange={handleEmail} />
+            <button onClick={handleLogin}>Login</button>
             
             <h2>Listings</h2>
             <input value={message} onChange={handleMessageUpdate}/>
+            <input value={type} onChange={handleTypeUpdate}/>
+            <input value={price} onChange={handlePriceUpdate}/>
+            <input value={title} onChange={handleTitleUpdate}/>
             <button onClick={handleSubmit}>Submit</button>
 
             <div>
